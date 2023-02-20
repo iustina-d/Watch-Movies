2c454015c60218cdb7bd21eeff817c1e8e85bfd9
iustina-d <iustina2dulea@gmail.com>
2023-02-20 16:15:03 +0200

    make it fully responsible

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ ball.addEventListener("click", () => {
   ball.classList.toggle("active")
 })
 
+const arrows = document.querySelectorAll(".arrow")
+const movieLists = document.querySelectorAll(".movie-list")
+
+arrows.forEach((arrow, i) => {
+  const itenNumber = movieLists[i].querySelectorAll("img").length
+  let clickCounter = 0
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270)
+    console.log(ratio)
+    console.log(itenNumber)
+    clickCounter++
+    let value
+    if (ratio === 6) {
+      value = 6
+    } else if (ratio > 2 && ratio < 6) {
+      value = 5
+    } else {
+      value = 4
+    }
+    if (itenNumber - (4 + clickCounter) - (ratio - value) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 320
+      }px)`
+    } else {
+      movieLists[i].style.transform = "translateX(0)"
+      clickCounter = 0
+    }
+  })
+})
